fe231a8aa261918e22d98d04822c5086a0ecf52f
Ihab Arnus <ihabarnus@gmail.com>
2024-09-16 18:07:27 +0300

    s3 upload

--- .github/actions/deploy/main.js
+++ .github/actions/deploy/main.js
@@ -1,8 +1,16 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const exec = require("@actions/exec");
+const shell = require("@actions/exec");
 
 function run() {
+  // 1) get intput values
+  const bucket = core.getInput("bucket", { required: true });
+  const region = core.getInput("bucket-region", { required: true });
+  const folder = core.getInput("build-folder", { required: true });
+
+  // 2) upload files
+  const s3Uri = `s3://${bucket}`;
+  shell.exec(`echo "aws s3 sync ${folder} ${s3Uri} --region ${region}"`);
   core.notice("Hello world!");
 }
 
