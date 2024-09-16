6a1c530f54c2a53f7cd8ffef70691ea7a9268416
Ihab Arnus <ihabarnus@gmail.com>
2024-09-16 18:33:29 +0300

    docker action

---
+++ .github/actions/docker/deployment.py
@@ -0,0 +1,12 @@
+import os
+
+def run():
+  print ("HELLO WORLD!")
+  print(os.environ['INPUT_BUCKET'])
+  print(os.environ['INPUT_REGION'])
+  print(os.environ['INPUT_BUILD-FOLDER'])
+  url = f'http://{os.environ['INPUT_BUCKET']}.s3-website-{os.environ['INPUT_REGION']}.amazonaws.com'
+  print(f'::set-output name=url::{url}')
+
+if __name__ == '__main__':
+  run()
