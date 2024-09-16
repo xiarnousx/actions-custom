ca7e4a5990ebf5de0453015317a81886af0f26ca
Ihab Arnus <ihabarnus@gmail.com>
2024-09-16 18:36:05 +0300

    fix typo

--- .github/actions/docker/deployment.py
+++ .github/actions/docker/deployment.py
@@ -3,7 +3,7 @@ import os
 def run():
   print ("HELLO WORLD!")
   print(os.environ['INPUT_BUCKET'])
-  print(os.environ['INPUT_REGION'])
+  print(os.environ['INPUT_BUCKET-REGION'])
   print(os.environ['INPUT_BUILD-FOLDER'])
   url = f'http://{os.environ['INPUT_BUCKET']}.s3-website-{os.environ['INPUT_REGION']}.amazonaws.com'
   print(f'::set-output name=url::{url}')
