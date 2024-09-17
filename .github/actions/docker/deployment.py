9fe91bc598c1b8a8f95904c03b34968bcece0351
Ihab Arnus <ihabarnus@gmail.com>
2024-09-17 05:54:11 +0300

    fix typo2

--- .github/actions/docker/deployment.py
+++ .github/actions/docker/deployment.py
@@ -5,7 +5,7 @@ def run():
   print(os.environ['INPUT_BUCKET'])
   print(os.environ['INPUT_BUCKET-REGION'])
   print(os.environ['INPUT_BUILD-FOLDER'])
-  url = f'http://{os.environ['INPUT_BUCKET']}.s3-website-{os.environ['INPUT_REGION']}.amazonaws.com'
+  url = f'http://{os.environ['INPUT_BUCKET']}.s3-website-{os.environ['INPUT_BUCKET-REGION']}.amazonaws.com'
   print(f'::set-output name=url::{url}')
 
 if __name__ == '__main__':
